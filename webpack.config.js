d6bf90a121f03ab8d2d4692029a8c0e51efe2544
Dong Uk Kang <gusrn1255@naver.com>
2022-07-09 02:21:39 +0900

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,60 @@
+const webpack = require("webpack");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+const port = 3000;
+
+module.exports = {
+  mode: "development",
+  entry: "./src/index.js",
+  output: {
+    path: __dirname + "/dist",
+    filename: "bundle.[hash].js",
+    publicPath: "/",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(png|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
+      },
+      {
+        test: /\.(js)$/,
+        exclude: /node_modules/,
+        use: ["babel-loader"],
+      },
+      {
+        test: /\.css$/,
+        use: [
+          {
+            loader: "style-loader",
+            options: {
+              esModule: false,
+            },
+          },
+          {
+            loader: "css-loader",
+            options: {
+              modules: {
+                exportOnlyLocals: true,
+                exportLocalsConvention: "camelCase",
+              },
+            },
+          },
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new HtmlWebpackPlugin({
+      template: "public/index.html",
+    }),
+  ],
+  devServer: {
+    host: "localhost",
+    port: port,
+    open: true,
+    historyApiFallback: true,
+    hot: true,
+  },
+};
