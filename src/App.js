679c18eb4e9aa43f4316f3e143684dc0fbd4fd88
Dong Uk Kang <gusrn1255@naver.com>
2022-07-09 02:42:25 +0900

    feat: route

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import React from "react";
 import { hot } from "react-hot-loader";
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import check_out from "./pages/check_out";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
 
 const App = () => {
     return (
-        <div>
-            <h1>Hello, World</h1>
-        </div>
+        <Routes>
+            <Route path="/" exact={true} component={Login} />
+            <Route path="/register" exact={true} component={Register} />
+            <Route path="/home" exact={true} component={Home} />
+        
+        </Routes>
     )
 };
 
