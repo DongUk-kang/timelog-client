fdff4cb8984812e5f36ea2af620942de077285c2
gusrn1255@naver.com <gusrn1255@naver.com>
2022-07-10 11:16:01 +0900

    feat: init tailwind

--- src/App.js
+++ src/App.js
@@ -1,17 +1,20 @@
 /* eslint-disable no-undef */
 import React from "react";
 import { hot } from "react-hot-loader";
-// import { BrowserRouter,  Route, Routes } from 'react-router-dom'
-// import Home from "./pages/Home";
-// import Login from "./pages/Login";
-// import Register from "./pages/Register";
+import { BrowserRouter as Router,  Route, Switch, Link, BrowserRouter } from 'react-router-dom'
+import Home from "./pages/Home";
+import Login from './pages/Login';
+import Register from "./pages/Register";
 
 const App = () => {
     return (
-        
-            <div>
-                <h1>Hello, World</h1>
-            </div>
+        <BrowserRouter>
+            <Switch>
+                <Route path='/' component={() => <Login />} />
+                <Route path="/home" component={() => <Home />}/>
+                <Route path="register" component={() => <Register />}/>
+            </Switch>
+        </BrowserRouter>
     )
 };
 
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,26 +1,30 @@
-// import React, { useState, useEffect } from "react";
-// import axios from "axios";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import "tailwindcss/tailwind.css"
 
-// const Login = () => {
+const Login = () => {
 
-//     const [email, setEmail] = useState('');
-//     const [password, setPassword] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
 
-//     const submitHandler = e => {
-//         e.preventDefault()
+    const submitHandler = e => {
+        e.preventDefault()
 
-//         const userInput = {
-//             email,
-//             password
-//         }
-//         console.log(userInput)
-//     }
+        const userInput = {
+            email,
+            password
+        }
+        console.log(userInput)
+    }
 
-//     return (
-//         <div>
-//             <h1>Login</h1>
-//         </div>
-//     )
-// };
+    return (
+        <div>
+            <span class="letter-spacing: -0.02em">Welcome </span>
+            <h4> Sign in below </h4>
+            <button>Sign In</button>
+            <button>Sign Up</button>
+        </div>
+    )
+};
 
-// export default Login;
+export default Login;
